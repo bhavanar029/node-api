9759484c2fc1c43410620c10800ed6688b43e130
Bhavana <bhavanar029@gmail.com>
2024-10-23 16:19:21 +0530

    arrays.js

--- arrays.js
+++ arrays.js
@@ -1,2 +1,9 @@
-let names=["A","B","C"]
-console.log(names)
+//let names=["A","B","C"];
+//console.log(names);
+
+//let animal=["cat","rat","dog","monkey"];
+//console.log(animal[2])
+
+let animal=["cat","rat","dog","monkey"];
+ animal[4]="bat";
+ console.log(animal);
